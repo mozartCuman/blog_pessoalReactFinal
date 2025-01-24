b76c31f86725e1e2075ef25a49bbcee87581f561
mozartCuman <mozartcumana@gmail.com>
2025-01-23 21:58:24 -0300

    Adicao de elemento sair

--- blog_pessoalFront/src/components/navbar/Navbar.tsx
+++ blog_pessoalFront/src/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import {Link} from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+    
     return (
         <>
             <div className='w-full flex justify-center py-4
@@ -14,7 +28,7 @@ function Navbar() {
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
-                        Sair
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
